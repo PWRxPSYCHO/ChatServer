2d15fbb67cbf8cd4c839134961e7d93d766430e9
PWRxPSYCHO <32745243+PWRxPSYCHO@users.noreply.github.com>
2018-11-24 13:31:52 -0500

    Adding more codeeee

--- ChatServer.py
+++ ChatServer.py
@@ -10,13 +10,26 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
 
+
 def incoming_clients():
     while True:
         client, address = serverSocket.accept()
         print("%s has connected" % address)
-        client.send(bytes("Hello! Welcome to the server! Type your username and press enter"))
+        client.send(
+            bytes("Hello! Welcome to the server! Type your username and press enter"))
         addresses[client] = address
         Thread(targer=handle_client, args=(client,)).start()
         print("hello world")
 
-        
+
+def handle_client():
+    print("This function will handle each individual client")
+
+
+if __name__ == "__main__":
+    serverSocket.listen(5)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=incoming_clients)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    serverSocket.close()
