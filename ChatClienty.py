e218f59c2fc5b526e51c7dc2c9053edb91b447c0
PWRxPSYCHO <32745243+PWRxPSYCHO@users.noreply.github.com>
2018-12-08 09:19:38 -0500

    Added receive function and connection variables

--- ChatClienty.py
+++ ChatClienty.py
@@ -1,4 +1,22 @@
-print ("hi")
-print("Lexie Test")
-print("Jackson Test on Desktop")
-print("Testing")
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+
+msg_list = []
+def receive():
+    while True:
+        try:
+            msg = client_socket.recv(buffer_size).decode("utf8")
+            msg_list.append(msg)
+        except OSError:
+            break
+
+
+
+buffer_size = 1024
+ADDR = ('localhost', 33000)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
