592b9b0332455dee52ea343cdf8158211b816411
PWRxPSYCHO <32745243+PWRxPSYCHO@users.noreply.github.com>
2018-12-09 11:37:47 -0500

    Cleaned up Server and Client class. 85% complete

--- ChatClienty.py
+++ ChatClienty.py
@@ -1,16 +1,29 @@
 from socket import *
+from threading import Thread
 
 serverName = 'localhost'
-serverPort = 6000
+serverPort = 5000
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-while True:
-    message = input("Enter your username:")
-    clientSocket.send(message.encode())
-    if message == "Quit":
-        break
-    else:
-        modifiedSentence = clientSocket.recv(1024)
-        print(modifiedSentence.decode())
+
+def incoming_messages():
+    while True:
+        try:
+            message = clientSocket.recv(1024)
+            print(message.decode())
+        except OSError:
+            break
+
+
+def client_messages():
+    clientMsg = input("")
+    clientSocket.send(clientMsg.encode())
+    if clientMsg == "quit":
+        clientSocket.close()
+
+
+if __name__ == "__main__":
+    receive_thread = Thread(target=incoming_messages)
+    receive_thread.start()
--- ChatServer.py
+++ ChatServer.py
@@ -1,63 +1,53 @@
-#!/usr/bin/env python3
-"""Server for multithreaded (asynchronous) chat application."""
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
-
-def accept_incoming_connections():
-    """Sets up handling for incoming clients."""
-    while True:
-        client, client_address = SERVER.accept()
-        print("%s:%s has connected." % client_address)
-        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
-        addresses[client] = client_address
-        Thread(target=handle_client, args=(client,)).start()
+serverPort = 5000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
 
 
-def handle_client(client):  # Takes client socket as argument.
-    """Handles a single client connection."""
-
-    name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-    client.send(bytes(welcome, "utf8"))
-    msg = "%s has joined the chat!" % name
-    broadcast(bytes(msg, "utf8"))
-    clients[client] = name
-
-    while True:
-        msg = client.recv(BUFSIZ)
-        if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, name+": ")
-        else:
-            client.send(bytes("{quit}", "utf8"))
-            client.close()
-            del clients[client]
-            broadcast(bytes("%s has left the chat." % name, "utf8"))
-            break
-
-
-def broadcast(msg, prefix=""):  # prefix is for name identification.
-    """Broadcasts a message to all the clients."""
-
-    for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
-
-        
 clients = {}
 addresses = {}
 
-HOST = ''
-PORT = 33000
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
 
-SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.bind(ADDR)
+def client_connections():
+    while True:
+        connection_socket, addr = serverSocket.accept()
+        print("%s:%s has connected" % addr)
+        connection_socket.send(bytes("Type your name: ", "utf8"))
+        addresses[connection_socket] = addr
+        Thread(target=handle_client, args=(connection_socket,)).start()
+
+
+def handle_client(connection_socket):
+    name = connection_socket.recv(1024).decode()
+    welcome_message = "Welcome %s. To exit, type Quit." % name
+    connection_socket.send(bytes(welcome_message, "utf8"))
+    message = "%s has joined!" % name
+    broadcast(bytes(message, "utf8"))
+    clients[connection_socket] = name
+
+    while True:
+        message = connection_socket.recv(1024).decode()
+        if message != bytes("Quit", "utf8"):
+            broadcast(message, name+": ")
+        else:
+            connection_socket.send(bytes(message, "utf8"))
+            connection_socket.close()
+            del clients[connection_socket]
+            broadcast(bytes("%s has left the chat" % name, "utf8"))
+            break
+
+
+def broadcast(msg, prefix=""):
+    for connections in clients:
+        connections.send(bytes(prefix, "utf8")+msg)
+
 
 if __name__ == "__main__":
-    SERVER.listen(5)
-    print("Waiting for connection...")
-    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    serverSocket.listen(5)
+    print("The server is ready to receive")
+    ACCEPT_THREAD = Thread(target=client_connections)
     ACCEPT_THREAD.start()
     ACCEPT_THREAD.join()
-    SERVER.close()
+    serverSocket.close()
