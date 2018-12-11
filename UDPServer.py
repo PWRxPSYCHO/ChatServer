050c29f29928126b58012e8f77e8be134b81fd8f
PWRxPSYCHO <32745243+PWRxPSYCHO@users.noreply.github.com>
2018-12-10 20:34:31 -0500

    Removed blocking, and made sure the data types were correct: data addr

--- UDPServer.py
+++ UDPServer.py
@@ -6,24 +6,19 @@ serverPort = 5000
 addr = (serverHost, serverPort)
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(addr)
-serverSocket.setblocking(0)
+#serverSocket.setblocking(0)
 
 clients = []
 isRunning = True
 print('Server Started')
 
 while isRunning:
-    try:
-        data, addr = serverSocket.recvfrom(2048)
-        message = data.decode()
-        address = addr.decode()
-        if "!quit" in message:
-            isRunning = False
-        if addr not in clients:
-            clients.append(address)
-        print(address + ":" + message)
-        for client in clients:
-            serverSocket.sendto(data, client)
-    except:
-        pass
+    data, addr = serverSocket.recvfrom(1024)
+    if "!quit" in str(data):
+        isRunning = False
+    if addr not in clients:
+        clients.append(addr)
+    print(str(addr) + ":" + str(data))
+    for client in clients:
+        serverSocket.sendto(data, client)
 serverSocket.close()
